f5c7b0e511d3a10ad4788fa7278acef22c27f5ba
Zonnaslyric <echezonnachukwu@gmail.com>
2021-03-23 18:55:24 -0700

    range calculator

--- RangeArray.js
+++
@@ -1,5 +0,0 @@
-function range(start, end) {
-    return Array(end - start + 1).fill().map((_, idx) => start + idx)
-  };
-  
-  console.log(range(9, 18));
--- Simple.js
+++ Simple.js
@@ -8,3 +8,12 @@
 //Count to 10 in an Array
 {[...Array(11)].map((e, i) => i)}
 //Result => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+//Range Calculator
+function range(start, end) {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+  };
+  
+  console.log(range(9, 18));
+//Result =>  [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
